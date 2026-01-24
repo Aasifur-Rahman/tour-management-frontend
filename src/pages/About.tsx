336b26954afc2317e71e4190a44e7181ab6b5524
Siam Rahman <asifur.web@gmail.com>
2026-01-24 20:51:17 +0600

    frontend layouting done

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,14 @@
-
-
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-16 px-4 container mx-auto">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam dolor nisi
+        qui expedita eligendi voluptatibus soluta ducimus saepe dolorum
+        exercitationem?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Velit delectus facilis eveniet perspiciatis, at quia architecto sequi
+        eaque modi sed.
+      </p>
+    </div>
+  );
 }
