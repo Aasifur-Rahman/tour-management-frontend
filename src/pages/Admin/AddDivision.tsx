745ca910165554b0feeb39e0c16ab97aac16427a
Aasifur-Rahman <asifur.web@gmail.com>
2026-05-09 21:30:18 +0600

    added divison options

--- src/pages/Admin/AddDivision.tsx
+++ src/pages/Admin/AddDivision.tsx
@@ -1,7 +1,10 @@
-
+import { AddDivisionModal } from "@/components/modules/Admin/Division/AddDivisionModal";
 
 export default function AddDivision() {
   return (
-    <div>AddDivision</div>
-  )
+    <div>
+      <h1>This is Add Division</h1>
+      <AddDivisionModal />
+    </div>
+  );
 }
--- src/routes/adminSidebarItems.ts
+++ src/routes/adminSidebarItems.ts
@@ -22,9 +22,9 @@ export const adminSidebarItems: ISidebarItem[] = [
     title: "Tour Management",
     items: [
       {
-        title: "Add Tour",
-        url: "/admin/add-tour",
-        component: AddTour,
+        title: "Add Division",
+        url: "/admin/add-division",
+        component: AddDivision,
       },
       {
         title: "Add Tour Type",
@@ -32,9 +32,9 @@ export const adminSidebarItems: ISidebarItem[] = [
         component: AddTourType,
       },
       {
-        title: "Add Division",
-        url: "/admin/add-division",
-        component: AddDivision,
+        title: "Add Tour",
+        url: "/admin/add-tour",
+        component: AddTour,
       },
     ],
   },
